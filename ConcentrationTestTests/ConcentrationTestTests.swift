af3f3c6e8a58a71a12690cb3790455a60ea56ddd
Navneet Singh <babra@usc.edu>
2018-05-21 01:14:24 -0700

    Initial Commit

---
+++ ConcentrationTestTests/ConcentrationTestTests.swift
@@ -0,0 +1,36 @@
+//
+//  ConcentrationTestTests.swift
+//  ConcentrationTestTests
+//
+//  Created by Navneet Babra on 5/21/18.
+//  Copyright © 2018 Navneet Babra. All rights reserved.
+//
+
+import XCTest
+@testable import ConcentrationTest
+
+class ConcentrationTestTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
